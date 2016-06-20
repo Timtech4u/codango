6b365e589d4771868da596a555b9599fcc12497c
Stanley Ndagi <stanley.ndagi@andela.com>
2016-06-20 16:29:12 +0300

    [Feature #116191173] Update team page

--- codango/static/js/components/team.jsx
+++ codango/static/js/components/team.jsx
@@ -88,7 +88,77 @@ var Team = React.createClass({
                                 <img src="https://avatars2.githubusercontent.com/u/13224175?v=3&s=460" className="img-responsive"/>
                             </div>
                             <div className="team-desc">
-                                <a href="https://github.com/andela-ooshodi"><h4>Olufunmilade Oshodi</h4></a>
+                                 <a href="https://github.com/andela-cnnadi"><h4>Chidiebere Nnadi</h4></a>
+                                <span>Team Lead</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                      <div className="team-members">
+                          <div className="team-avatar">
+                             <img src="https://avatars2.githubusercontent.com/u/15629602?v=3&s=460" className="img-responsive"/>
+                          </div>
+                          <div className="team-desc">
+                               <a href="https://github.com/andela-sndagi"><h4>Stanley Ndagi</h4></a>
+                              <span>Developer</span>
+                          </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="team-members">
+                            <div className="team-avatar">
+                               <img src="https://avatars2.githubusercontent.com/u/12407721?v=3&s=400" className="img-responsive"/>
+                            </div>
+                            <div className="team-desc">
+                                 <a href="https://github.com/andela-mochieng"><h4>Margaret Ochieng</h4></a>
+                                <span>Developer</span>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-4">
+                      <div className="team-members">
+                          <div className="team-avatar">
+                             <img src="https://avatars3.githubusercontent.com/u/17270426?v=3&s=460" className="img-responsive"/>
+                          </div>
+                          <div className="team-desc">
+                               <a href="https://github.com/andela-aabdulwahab"><h4>Abdulmalik Abdulwahab</h4></a>
+                              <span>Developer</span>
+                          </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="team-members">
+                            <div className="team-avatar">
+                               <img src="https://avatars2.githubusercontent.com/u/17288133?v=3&s=460" className="img-responsive"/>
+                            </div>
+                            <div className="team-desc">
+                                 <a href="https://github.com/andela-akiura"><h4>Alex Kiura</h4></a>
+                                <span>Developer</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="team-members">
+                            <div className="team-avatar">
+                               <img src="https://avatars0.githubusercontent.com/u/18328313?v=3&s=460" className="img-responsive"/>
+                            </div>
+                            <div className="team-desc">
+                                 <a href="https://github.com/andela-snwuguru"><h4>Nwuguru Sunday</h4></a>
+                                <span>Developer</span>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-4">
+                        <div className="team-members">
+                            <div className="team-avatar">
+                               <img src="https://avatars1.githubusercontent.com/u/18309948?v=3&s=460" className="img-responsive"/>
+                            </div>
+                            <div className="team-desc">
+                                 <a href="https://github.com/andela-hoyeboade"><h4>Hassan Oyeboade</h4></a>
                                 <span>Developer</span>
                             </div>
                         </div>
@@ -119,7 +189,7 @@ var Team = React.createClass({
                                <img src="https://avatars2.githubusercontent.com/u/15088852?v=3&s=460" className="img-responsive"/>
                             </div>
                             <div className="team-desc">
-                                 <a href="https://github.com/andela-ashuaib"><h4>Abiodun Shuaib</h4></a>
+                                <a href="https://github.com/andela-ooshodi"><h4>Olufunmilade Oshodi</h4></a>
                                 <span>Developer</span>
                             </div>
                         </div>
@@ -146,10 +216,10 @@ var Team = React.createClass({
                                  <a href="https://github.com/andela-ashuaib"><h4>Abiodun Shuaib</h4></a>
                         <div className="team-members">
                             <div className="team-avatar">
-                               <img src="https://avatars3.githubusercontent.com/u/13269579?v=3&s=460" className="img-responsive"/>
+                               <img src="https://avatars2.githubusercontent.com/u/15088852?v=3&s=460" className="img-responsive"/>
                             </div>
                             <div className="team-desc">
-                                 <a href="https://github.com/andela-jngatia"><h4>Joan Ngatia</h4></a>
+                                 <a href="https://github.com/andela-ashuaib"><h4>Abiodun Shuaib</h4></a>
                                 <span>Developer</span>
                             </div>
                         </div>
@@ -164,7 +234,7 @@ var Team = React.createClass({
                                <img src="https://avatars2.githubusercontent.com/u/15629602?v=3&s=460" className="img-responsive"/>
                             </div>
                             <div className="team-desc">
-                                 <a href="https://github.com/NdagiStanley"><h4>Stan MD</h4></a>
+                                 <a href="https://github.com/andela-jngatia"><h4>Joan Ngatia</h4></a>
                                 <span>Developer</span>
                             </div>
                         </div>
@@ -179,13 +249,14 @@ var Team = React.createClass({
                                <img src="https://avatars2.githubusercontent.com/u/17288133?v=3&s=460" className="img-responsive"/>
                             </div>
                             <div className="team-desc">
-                                 <a href="https://github.com/andela-akiura"><h4>Alex Kiura</h4></a>
+                                 <a href="https://github.com/IniOluwa"><h4>Ini-Oluwa C. Fageyinbo</h4></a>
                                 <span>Developer</span>
                             </div>
                         </div>
                     </div>
                   </div>
                 </div>
+
             </div>
         </div>
 
