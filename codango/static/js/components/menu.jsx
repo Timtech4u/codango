86df6fee65a0a391cedc5b2cfbe3a45e90dfeb91
Stanley Ndagi <stanley.ndagi@andela.com>
2016-07-04 17:36:30 +0300

    [Feature #116191173] Complete e2e tests

--- codango/static/js/components/menu.jsx
+++ codango/static/js/components/menu.jsx
@@ -20,8 +20,8 @@ export default class Menu extends Component {
                         </div>
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul className="nav navbar-nav navbar-right">
-                                <li><Link to="/about">About</Link></li>
-                                <li><Link to="/contact">Contact us</Link></li>
+                                <li><Link to="/about">About Us</Link></li>
+                                <li><Link to="/contact">Contact Us</Link></li>
                                  <li><Link to="/team">Team</Link></li>
                             </ul>
                         </div>
