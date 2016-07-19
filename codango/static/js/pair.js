757a87917a1927cf34bdbb82e57ab991f05b5fb3
Stanley Ndagi <stanley.ndagi@andela.com>
2016-07-19 15:15:24 +0300

    [Feature #119257909] JavaScript rearrangement

--- codango/static/js/pair.js
+++ codango/static/js/pair.js
@@ -26,6 +26,7 @@ $(document).ready(function () {
       }
       var theme = selectedTheme || save_theme || 'cobalt';
       var language = selectedLanguage || 'python';
+
       // Get current session firebase ref
       editor = ace.edit('firepad-container');
       session = editor.getSession();
@@ -40,10 +41,8 @@ $(document).ready(function () {
     bindEvents: function () {
       // Language change event handler
       $('#language').change(function () {
-        var lang = $(this).val();
-        var theme = $('#theme').val();
-        changeEditorOption(lang, theme);
-        app.updateSession(lang);
+
+        changeEditorOption($(this).val(), $('#theme').val());
       });
 
       // Theme change event handler
@@ -53,6 +52,7 @@ $(document).ready(function () {
         changeEditorOption(lang, theme);
 
         localStorage.setItem('theme', theme)
+
       });
     },
     getPageRef: function () {
