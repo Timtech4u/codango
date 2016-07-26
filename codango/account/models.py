12f1f8de22afa1657501b3408c4ec9c3e3049f50
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-07-26 11:11:12 +0100

    [Feature #118370581] Modify Cotact us model

--- codango/account/models.py
+++ codango/account/models.py
@@ -6,7 +6,7 @@ Models for contact us
 class ContactUsModel(models.Model):
     name = models.CharField(max_length=100, null=False)
     email = models.CharField(max_length=100, null=False)
-    subject = models.CharField(max_length=100, null=False)
+    subject = models.CharField(max_length=100, null=True)
     message = models.TextField(null=False)
     date_sent = models.DateTimeField(auto_now_add=True)
 
