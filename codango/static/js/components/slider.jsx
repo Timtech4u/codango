c3c5d88353a50101f75a259525419dffea4a2b5c
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-07-27 15:54:44 +0100

    [feature #119188135] add get stated button to the carousel

--- codango/static/js/components/slider.jsx
+++ codango/static/js/components/slider.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Carousel} from 'react-bootstrap';
+import {Carousel, Button} from 'react-bootstrap';
 
 export default class Slider extends Component {
     render() {
@@ -10,6 +10,9 @@ export default class Slider extends Component {
                             <Carousel.Caption>
                                 <h3>Collaborate</h3>
                                 <p>Work with multiple members of your team at the same time in one code editor.</p>
+                                <Button  bsSize="large" className="get-stated-btn">
+                                    Get Started today<i className="mdi mdi-trending-neutral"></i>
+                                </Button>
                             </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
@@ -17,6 +20,9 @@ export default class Slider extends Component {
                             <Carousel.Caption>
                                 <h3>Second slide label</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <Button  bsSize="large" className="get-stated-btn">
+                                    Get Started today<i className="mdi mdi-trending-neutral"></i>
+                                </Button>
                             </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
@@ -24,6 +30,9 @@ export default class Slider extends Component {
                             <Carousel.Caption>
                                 <h3>Third slide label</h3>
                                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                <Button  bsSize="large" className="get-stated-btn">
+                                    Get Started today<i className="mdi mdi-trending-neutral"></i>
+                                </Button>
                             </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
