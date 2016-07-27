94985dfd4458116db712d5fbf2c963f795c38847
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-07-27 12:05:08 +0100

    [Fix #119188135] fix Carousel rendering

--- codango/static/js/components/slider.jsx
+++ codango/static/js/components/slider.jsx
@@ -4,32 +4,29 @@ import {Carousel} from 'react-bootstrap';
 export default class Slider extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="slide-container">
-                    <div className="slide-content">
-                        <Carousel>
+                        <Carousel className="slide-container">
                             <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="static/img/laptop.jpg"/>
                             <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                <h3>Collaborate</h3>
+                                <p>Work with multiple members of your team at the same time in one code editor.</p>
                             </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="static/img/laptop.jpg"/>
                             <Carousel.Caption>
                                 <h3>Second slide label</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="static/img/laptop.jpg"/>
                             <Carousel.Caption>
                                 <h3>Third slide label</h3>
                                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                             </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
-                    </div>
-                </div>
-            </div>
         )
     }
 }
