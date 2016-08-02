0881483273b40503234dd3afadafeca68b92ac8a
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-08-02 10:59:01 +0100

    [Fix #118370581] Handle request and status code errors

--- codango/static/js/components/contact.jsx
+++ codango/static/js/components/contact.jsx
@@ -17,7 +17,7 @@ import {
 } from 'react-bootstrap';
 
 class Contact extends Component {
-    constructor() {
+  constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFieldChange = this.handleFieldChange.bind(this);
@@ -25,121 +25,120 @@ class Contact extends Component {
     this.displayFlashMessage = this.displayFlashMessage.bind(this);
     this.clearTextFields = this.clearTextFields.bind(this);
     this.state = {
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-        flashMessage: '',
-        displayFlashMessage: 'none',
-        messageType: 'success'
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+      flashMessage: '',
+      displayFlashMessage: 'none',
+      messageType: 'success'
+      }
+  }
+
+  handleFieldChange(event) {
+    event.preventDefault();
+    let key = event.target.name;
+    let value = event.target.value;
+    this.setState({
+        [key]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.sendMessage(this.state.name, this.state.email,
+      this.state.subject, this.state.message)
+  }
+
+  clearTextFields() {
+    this.setState({name: '', email: '', subject:'', message:''});
+  }
+
+  displayFlashMessage(message, messageType) {
+    this.setState({messageType: messageType,
+                    flashMessage: message,
+                    displayFlashMessage: "block",
+                  });
+    setTimeout(function(){
+      this.setState({flashMessage: "",
+                      displayFlashMessage: "none"});
+    }.bind(this), 3000);
+  }
+
+  sendMessage(name, email, subject, message) {
+    request
+      .post('/api/v1/contact/')
+      .type('form')
+      .send({'name': name, 'email': email, 'subject': subject,
+             'message': message })
+      .end((err, result) => {
+        if(err || !(result.status === 201)) {
+          this.displayFlashMessage("Message not sent", "danger")
+        } else {
+          this.displayFlashMessage("Your message has been successfully sent", "success")
+          this.clearTextFields()
         }
-    }
-
-    handleFieldChange(event) {
-        event.preventDefault();
-        let key = event.target.name;
-        let value = event.target.value;
-        this.setState({
-            [key]: value
-        });
-    }
-
-    handleSubmit(event) {
-      event.preventDefault();
-      this.sendMessage(this.state.name, this.state.email,
-        this.state.subject, this.state.message)
-    }
-
-    clearTextFields() {
-      this.setState({name: '', email: '', subject:'', message:''});
-    }
-    
-    displayFlashMessage(message, messageType) {
-      this.setState({messageType: messageType,
-                      flashMessage: message,
-                      displayFlashMessage: "block",
-                    });
-      setTimeout(function(){
-        this.setState({flashMessage: "",
-                        displayFlashMessage: "none"});
-      }.bind(this), 3000);
-    }
-
-    sendMessage(name, email, subject, message) {
-      request
-        .post('/api/v1/contactus/')
-        .send({'name': name, 'email': email, 'subject': subject,
-               'message': message })
-        .end((err, result) => {
-          if (result.status === 201) {
-            this.displayFlashMessage(result.body.message, "success")
-            this.clearTextFields()
-          }
-          else {
-            this.displayFlashMessage("Unable to send message", "danger")
-          }
-
-        });
-    }
+      });
+  }
   render() {
-        return (
-            <div>
-              <div className="row">
-                <div className="col-sm-12 col-lg-12">
-                    <h1 className="page-title">Contact us <small><em>We'd love to hear from you</em></small></h1>
-                </div>
-              </div>
-              <div className="row">
-                  <div className="col-md-8">
-                    <div  className="well  well-sm">
-                      <Form horizontal action="post" onSubmit={this.handleSubmit} className="Contact">
-                          <Alert bsStyle={this.state.messageType} style={{"display": this.state.displayFlashMessage}}>
-                            {this.state.flashMessage}
-                          </Alert>
-                          <FormGroup controlId="formControlsText">
-                              <Col componentClass={ControlLabel} sm={2}>Name</Col>
-                               <Col sm={10}>
-                                   <FormControl type="text" required={true} placeholder="Enter fullname" name="name" value={this.state.name} onChange={this.handleFieldChange}/>
-                               </Col>
-                          </FormGroup>
-                          <FormGroup controlId="formControlsText">
-                              <Col componentClass={ControlLabel} sm={2}>Email</Col>
-                              <Col sm={10}>
-                                  <FormControl type="email" required={true} placeholder="john.doe@example.com" name="email" value={this.state.email} onChange={this.handleFieldChange}/>
-                              </Col>
-                          </FormGroup>
-                          <FormGroup controlId="formControlsText">
-                              <Col componentClass={ControlLabel} sm={2}>Subject</Col>
-                              <Col sm={10}>
-                                  <FormControl type="text" placeholder="Enter message subject" name="subject" value={this.state.subject} onChange={this.handleFieldChange}/>
-                              </Col>
-                          </FormGroup>
-                          <FormGroup controlId="formControlsText">
-                              <Col componentClass={ControlLabel} sm={2}>Message</Col>
-                              <Col sm={10}>
-                                  <FormControl componentClass="textarea" required={true} placeholder="Enter your message here" name="message" value={this.state.message} onChange={this.handleFieldChange}/>
-                              </Col>
-                          </FormGroup>
-                          <FormGroup controlId="formControlsText">
-                              <Col smOffset = {2} sm={2}>
-                                  <Button type="submit" id="contact" type="submit" className="btn btn-primary">Send Message</Button>
-                              </Col>
-                          </FormGroup>
-                    </Form>
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                      <address>
-                        <strong>Codango, Inc.</strong><br />
-                        55 Moleye Street<br />
-                        Sabo, Yaba<br />
-                        Nigeria<br />
-                        0800-codango
-                      </address>
-                  </div>
-                </div>
+    return (
+      <div>
+        <div className="row">
+          <div className="col-sm-12 col-lg-12">
+              <h1 className="page-title">Contact us <small><em>We'd love to hear from you</em></small></h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <div  className="well  well-sm">
+              <Form horizontal action="post" onSubmit={this.handleSubmit} className="Contact">
+                <Alert bsStyle={this.state.messageType} style={{"display": this.state.displayFlashMessage}}>
+                  {this.state.flashMessage}
+                </Alert>
+                <FormGroup controlId="formControlsText">
+                  <Col componentClass={ControlLabel} sm={2}>Name</Col>
+                  <Col sm={10}>
+                    <FormControl type="text" required={true} placeholder="Enter fullname" name="name" value={this.state.name} onChange={this.handleFieldChange}/>
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formControlsText">
+                  <Col componentClass={ControlLabel} sm={2}>Email</Col>
+                  <Col sm={10}>
+                      <FormControl type="email" required={true} placeholder="john.doe@example.com" name="email" value={this.state.email} onChange={this.handleFieldChange}/>
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formControlsText">
+                    <Col componentClass={ControlLabel} sm={2}>Subject</Col>
+                    <Col sm={10}>
+                        <FormControl type="text" placeholder="Enter message subject" name="subject" value={this.state.subject} onChange={this.handleFieldChange}/>
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="formControlsText">
+                  <Col componentClass={ControlLabel} sm={2}>Message</Col>
+                  <Col sm={10}>
+                      <FormControl componentClass="textarea" required={true} placeholder="Enter your message here" name="message" value={this.state.message} onChange={this.handleFieldChange}/>
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formControlsText">
+                  <Col smOffset = {2} sm={2}>
+                      <Button type="submit" id="contact" type="submit" className="btn btn-primary">Send Message</Button>
+                  </Col>
+                </FormGroup>
+              </Form>
             </div>
-        );
-    }
+          </div>
+          <div className="col-md-4">
+              <address>
+                <strong>Codango, Inc.</strong><br />
+                55 Moleye Street<br />
+                Sabo, Yaba<br />
+                Nigeria<br />
+                0800-codango
+              </address>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 module.exports = Contact;
