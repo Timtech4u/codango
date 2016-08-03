2c9513a81660d970b2615e3ebbf93d67e743c464
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-03 17:08:11 +0100

    [Feature #119188135] implement the contact section

--- codango/static/js/components/about.jsx
+++ codango/static/js/components/about.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import { Grid, Row } from 'react-bootstrap';
 import Features from './features.jsx';
+import Contact from './contact.jsx';
+import Team from './team.jsx';
 
 export default class About extends Component {
   render() {
     return (
       <div >
         <Grid className="header" />
-        <Grid className="content">
           <Features />
-        </Grid>
+          <Team />
+          <Contact />
       </div>
     )
   }
