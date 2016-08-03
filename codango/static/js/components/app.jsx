bdb0e03bd084aaa0c9204a6ca3a5c2d80326989e
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-03 12:56:34 +0100

    [Feature #119188135] create the about page

--- codango/static/js/components/app.jsx
+++ codango/static/js/components/app.jsx
@@ -4,12 +4,13 @@ import { browserHistory, IndexRoute, Router, Route } from 'react-router';
 import React from 'react';
 import Main from './main.jsx';
 import Home from './home.jsx';
+import About from './about.jsx';
 
 const routes = (<Router history={browserHistory}>
                 <Route path="/" component={Main}>
                     <IndexRoute component={Home} history={browserHistory}/>
-                    
-                </Route> 
+                    <Route path='/about' component={About} />
+                </Route>
               </Router>);
 
 render(routes, document.getElementById('react'));
