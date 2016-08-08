d1bbdf90f8fe390b935d8076008cbb203ad99332
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-08 16:25:05 +0100

    [Feature #119188135] style the footer

--- codango/static/js/components/footer.jsx
+++ codango/static/js/components/footer.jsx
@@ -4,31 +4,39 @@ import { Grid, Row, Col, Form, FormGroup, FormControl, InputGroup, Button} from 
 export default class Footer extends Component {
   render(){
     return(
-      <Grid className="static-footer">
-        <Row className="show-grid" >
-          <Col md={4} >
-            <p> Socialize with Codango </p>
-            <span></span>
-          </Col>
-          <Col md={4} >
-            <p><a href="#" >Term of use</a></p>
-            <p><a href="#" >Privacy Policy</a></p>
-          </Col>
-          <Col md={4} >
-            <p>Subscribe to Our news letter</p>
-            <Form >
-              <FormGroup>
-                <InputGroup>
-                  <FormControl type="text" />
-                    <InputGroup.Button>
-                      <Button>Subscribe</Button>
-                    </InputGroup.Button>
-                </InputGroup>
-              </FormGroup>
-            </Form>
-          </Col>
-        </Row>
-      </Grid>
+      <div className="static-footer" >
+        <Grid >
+          <Row className="show-grid" >
+            <Col md={4} >
+              <p> Socialize with Codango </p>
+              <a href="#"><i className="mdi mdi-facebook-box"></i></a>
+              <a href="#"><i className="mdi mdi-github-box"></i></a>
+              <a href="#"><i className="mdi mdi-twitter-box"></i></a>
+              <a href="#"><i className="mdi mdi-google-plus-box"></i></a>
+            </Col>
+            <Col md={4} >
+              <p><a href="#" >Term of use</a></p>
+              <p><a href="#" >Privacy Policy</a></p>
+            </Col>
+            <Col md={4} >
+              <p>Subscribe to our news letter</p>
+              <Form >
+                <FormGroup>
+                  <InputGroup>
+                    <FormControl type="text" />
+                      <InputGroup.Button>
+                        <Button type="submit" className="btn">Subscribe</Button>
+                      </InputGroup.Button>
+                  </InputGroup>
+                </FormGroup>
+              </Form>
+            </Col>
+          </Row>
+          <Row className="reserved" >
+              <p> ©2016 All Right reserved. Codango </p>
+          </Row>
+        </Grid>
+      </div>
     )
   }
 }
