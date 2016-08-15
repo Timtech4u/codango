24514dd1d15c9d4b5658dca4a7a8070f1964e69b
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-15 16:30:09 +0100

    [Feature #119188135] Add popular post section

--- codango/static/js/components/feeds.jsx
+++ codango/static/js/components/feeds.jsx
@@ -18,8 +18,16 @@ export default class Feeds extends Component {
               <div className="list-group" id="community">
                 <a href="#" className="list-group-item active"><i className="mdi mdi-view-list"></i> All Feeds</a>
                 <a href="#" className="list-group-item"><i className="mdi mdi-language-python"></i> Python</a>
-                <a href="/resource/ajax/community/android" className="list-group-item"><i className="mdi mdi-android"></i> Android</a>
-                <a href="/resource/ajax/community/javascript" className="list-group-item"><i className="mdi mdi-language-javascript"></i> Javascript</a>
+                <a href="#" className="list-group-item"><i className="mdi mdi-android"></i> Android</a>
+                <a href="#" className="list-group-item"><i className="mdi mdi-language-javascript"></i> Javascript</a>
+              </div>
+            </div>
+
+            <div className="popular-block">
+              <h3 className="section-header">Popular Resources</h3>
+              <div className="list-group" id="popular">
+              <Popular />
+              <Popular />
               </div>
             </div>
           </Col>
@@ -61,6 +69,19 @@ class Post extends Component {
           </div>
         </Col>
       </Row>
+    )
+  }
+}
+
+class Popular extends Component {
+  render () {
+    return(
+      <div className="list-group">
+        <a href="#" className="list-group-item">
+          <h5 className="list-group-item-heading">Posted by hassan <small><em>2&nbsp;months ago</em></small></h5>
+          <p className="list-group-item-text">Learn Django - the web framework for perfectionists with deadlines</p>
+        </a>
+      </div>
     )
   }
 }
