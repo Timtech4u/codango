ec1b14c43c6725eeef05091debb20c0bbf128ac7
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-16 04:32:03 +0100

    [Feature #119188135] Rearrange the submenu

--- codango/static/js/components/subMenu.jsx
+++ codango/static/js/components/subMenu.jsx
@@ -1,32 +1,32 @@
 import React, {Component} from 'react';
-import {Navbar, FormGroup, FormControl} from 'react-bootstrap';
+import {Grid, Row, Col, FormGroup, Glyphicon, Button, FormControl, DropdownButton, MenuItem} from 'react-bootstrap';
 
-export default class Menu extends Component {
-    render() {
-        return (
-            <Navbar className="search-bar">
-             <form className="search-form">
-                 <div className="col-md-6">
-                     <div className="form-group">
-                        <input type="text" className="form-control search-field h50" placeholder="Search" />
-                        <span className="glyphicon glyphicon-search"></span>
-                      </div>
-                 </div>
-                 <div className="col-md-6">
-                   <FormGroup controlId="formControlsSelect">
-                      <FormControl componentClass="select" placeholder="Filter"  className="h50">
-                        <option value="select">All filters</option>
-                        <option value="">Newest</option>
-                        <option value="">Oldest</option>
-                        <option value="">Most Engaging</option>
-                        <option value="">Highest Rated</option>
-                      </FormControl>
-                    </FormGroup>
-                 </div>
+export default class SubMenu extends Component {
+  render() {
+    return (
+        <Row className="submenu" >
+          <Col md={2} >
+            <DropdownButton title="Filter Post" id="bg-nested-dropdown">
+              <MenuItem eventKey="1">All</MenuItem>
+              <MenuItem eventKey="2">Newest</MenuItem>
+              <MenuItem eventKey="3">Most Rated</MenuItem>
+              <MenuItem eventKey="4">Trending</MenuItem>
+            </DropdownButton>
+          </Col>
+          <Col md={8} >
+            <form >
+              <FormGroup >
+                <FormControl type="text" placeholder="Search"/>
+                <FormControl.Feedback>
+                  <Glyphicon glyph="search" />
+                </FormControl.Feedback>
+              </FormGroup>
             </form>
-          </Navbar>
-
-        )
-    }
+          </Col>
+          <Col md={2} >
+            <Button className="login-btn pull-right">Login</Button>
+          </Col>
+        </Row>
+    )
+  }
 }
-module.exports = Menu;
