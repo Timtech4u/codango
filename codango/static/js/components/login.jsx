fae0d3cdc2ba2cd408ba970b324865f0b17fd9f1
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-16 14:48:48 +0100

    [Feature #119188135] Create signup modal

--- codango/static/js/components/login.jsx
+++ codango/static/js/components/login.jsx
@@ -9,7 +9,10 @@ import {
     Form,
     Link,
     Modal,
-    Col
+    Col,
+    Nav,
+    NavItem,
+    Tab
 } from 'react-bootstrap';
 
 
@@ -31,9 +34,9 @@ export default class Login extends Component {
   render() {
     return (
       <div>
-      <Button bsStyle="primary" onClick={this.open}>
-        Login / Sign Up
-      </Button>
+        <Button bsStyle="primary" onClick={this.open}>
+          Login / Sign Up
+        </Button>
         <Modal show={this.state.showModal} onHide={this.close} className="signin-modal">
           <Modal.Body>
             <h4 >Login with</h4>
@@ -42,25 +45,66 @@ export default class Login extends Component {
               <Button className="round-btn gplus-btn" ><i className="mdi mdi-google-plus"></i></Button>
             </div>
             <p className="or-divider"><span>or</span></p>
-            <Form >
-              <FormGroup >
-                <FormControl.Feedback>
-                  <i className="mdi mdi-account"></i>
-                </FormControl.Feedback>
-                <FormControl type="text" placeholder="Username" />
-              </FormGroup>
-              <FormGroup >
-                <FormControl.Feedback>
-                  <i className="mdi mdi-lock"></i>
-                </FormControl.Feedback>
-                <FormControl type="password" placeholder="Password" />
-              </FormGroup>
-              <Checkbox>Remember me</Checkbox>
-              <Button type="submit" block className="login-btn">
-                Login
-              </Button>
-            </Form>
-            <p className="account-message" >Don't have an account? <a href="#">Sign Up</a></p>  
+            <Tab.Container id="login-tab" defaultActiveKey="login">
+              <Tab.Content animation>
+                <Tab.Pane eventKey="login">
+                <Form >
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-account"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="text" placeholder="Username" />
+                  </FormGroup>
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-lock"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="password" placeholder="Password" />
+                  </FormGroup>
+                  <Checkbox>Remember me</Checkbox>
+                  <Button type="submit" block className="login-btn">
+                    Login
+                  </Button>
+                </Form>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="signup">
+                <Form >
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-account"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="text" placeholder="Username" />
+                  </FormGroup>
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-email-outline"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="text" placeholder="Email" />
+                  </FormGroup>
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-lock"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="password" placeholder="Password" />
+                  </FormGroup>
+                  <FormGroup >
+                    <FormControl.Feedback>
+                      <i className="mdi mdi-lock"></i>
+                    </FormControl.Feedback>
+                    <FormControl type="password" placeholder="Confim Password" />
+                  </FormGroup>
+                  <Button type="submit" block className="login-btn">
+                    Sign Up
+                  </Button>
+                </Form>
+                </Tab.Pane>
+                <Nav >
+                  <NavItem eventKey="signup"><span className="tab-nav-text">Don't have an account? </span>Sign Up</NavItem>
+                  <NavItem eventKey="login"><span className="tab-nav-text">Already have an account? </span>Login</NavItem>
+                </Nav>
+              </Tab.Content>
+            </Tab.Container>
           </Modal.Body>
         </Modal>
       </div>
