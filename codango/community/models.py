5074a00d8559a6780e791a6409c3c30da5d82676
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-08-17 12:53:09 +0100

    [Feature #128481605] Create timestamp mixin

---
+++ codango/community/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class TimeStampMixin(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
