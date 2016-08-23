16e0827acbc8db22a3b6b242239155c84b87066b
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-23 18:36:50 +0100

    [Chore #128842965] Indent the gulp file properly

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,32 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
-gulp.task('buildreact', function () {
-    return browserify({entries: 'codango/static/js/components/app.jsx', extensions: ['.jsx'], debug: true})
-        .transform('babelify', {presets: ['es2015', 'react']})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('codango/static/js/build'));
+gulp.task('buildreact', function() {
+  return browserify({
+      entries: 'codango/static/js/components/app.jsx',
+      extensions: ['.jsx'],
+      debug: true
+    })
+    .transform('babelify', {
+      presets: ['es2015', 'react']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('codango/static/js/build'));
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp.src('codango/static/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('codango/static/css'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch('codango/static/scss/*.scss', ['sass']);
   gulp.watch('codango/static/js/components/*.jsx', ['buildreact']);
 });
 
-gulp.task('default', function (callback) {
+gulp.task('default', function(callback) {
   runSequence(['sass', 'buildreact', 'watch'],
     callback
   );
