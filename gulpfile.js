9958a0064ab6b1f4244a31912425a9fa0fbb6ab1
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-23 12:31:09 +0100

    [Feature #128842965] setup gulp for react bundling

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('buildreact', function () {
+    return browserify({entries: 'codango/static/js/components/app.jsx', extensions: ['.jsx'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('codango/static/js/build'));
+});
 
 gulp.task('sass', function(){
   return gulp.src('codango/static/scss/*.scss')
@@ -10,10 +21,11 @@ gulp.task('sass', function(){
 
 gulp.task('watch', function(){
   gulp.watch('codango/static/scss/*.scss', ['sass']);
-})
+  gulp.watch('codango/static/js/components/*.jsx', ['buildreact']);
+});
 
 gulp.task('default', function (callback) {
-  runSequence(['sass', 'watch'],
+  runSequence(['sass', 'buildreact', 'watch'],
     callback
-  )
-})
+  );
+});
