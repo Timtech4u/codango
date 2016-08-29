401ea5daa8cb498933bd0127695152a062e617b3
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-29 12:06:55 +0100

    [Fix #128843781] remove hash in url linking id to component

--- codango/static/js/components/app.jsx
+++ codango/static/js/components/app.jsx
@@ -10,8 +10,25 @@ import Contact from './contact.jsx';
 import MainNew from './main-new.jsx'
 import HomeNew from './home-new.jsx';
 
+function hashLinkScroll() {
+  const {hash} = window.location;
+  if (hash !== '') {
+    // Push onto callback queue so it runs after the DOM is updated, this is
+    // required when navigating from a different page so that the element is
+    // rendered on the page before trying to getElementById.
+    setTimeout(() => {
+      const id = hash.replace('#', '');
+      history.pushState("", document.title, window.location.pathname + window.location.search);
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView();
+      }
+    }, 0);
+  }
+}
+
 const routes = (
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={hashLinkScroll}>
     <Route path="/" component={Main}>
       <IndexRoute component={Home} history={browserHistory}/>
       <Route path="/team" component={Team}/>
