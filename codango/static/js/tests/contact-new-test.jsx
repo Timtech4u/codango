fe6afc695787e114a5482b8e3018e10e5d3d4099
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-20 05:38:47 +0100

    [Fix #128843583] Move all the contact us form element testing into one it block

--- codango/static/js/tests/contact-new-test.jsx
+++ codango/static/js/tests/contact-new-test.jsx
@@ -10,19 +10,10 @@ describe('<Contact />', () => {
     expect(wrapper.contains(<h3>Get In Touch</h3>)).to.equal(true);
   });
 
-  it('contains name field', () => {
+  it('renders contact us form', () => {
     expect(wrapper.find(FormControl).at(0).prop('name')).to.equal('name');
-  });
-
-  it('contains email field', () => {
     expect(wrapper.find(FormControl).at(1).prop('name')).to.equal('email');
-  });
-
-  it('contains subject field', () => {
     expect(wrapper.find(FormControl).at(2).prop('name')).to.equal('subject');
-  });
-
-  it('contains message field', () => {
     expect(wrapper.find(FormControl).at(3).prop('name')).to.equal('message');
   });
 });
