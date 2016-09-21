9ebe33e0b044625c7094e718e722e3df9c08fc14
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-21 03:03:04 +0100

    [Feature #130708987] Write test for the new footer page

---
+++ codango/static/js/tests/footer-new-test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const expect = require('expect');
+import { mount } from 'enzyme';
+import { Form } from 'react-bootstrap';
+import Footer from '../components/footer.jsx';
+
+describe('<Footer />', () => {
+  const wrapper = mount(<Footer />);
+  it('contains socialize message', () => {
+    expect(wrapper.text()).toContain('Socialize with Codango');
+  });
+  it('contains term of use', () => {
+    expect(wrapper.text()).toContain('Term of use');
+  })
+  it('has subscribe form', () => {
+    expect(wrapper.find(Form).length).toBe(1);
+  })
+});
