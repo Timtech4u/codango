5604850779276743309d57071d9d0bcf64202d0d
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-10-10 11:08:16 +0100

    [Fix #128843949] fix feed overlay on submenu

--- codango/static/js/components/SubMenu.jsx
+++ codango/static/js/components/SubMenu.jsx
@@ -23,29 +23,29 @@ export default class SubMenu extends Component {
   }
   render() {
     return (
-        <div className="submenu" >
-          <Col md={2} >
-            <DropdownButton title="Filter Posts" id="bg-nested-dropdown">
-              <MenuItem eventKey="1">All</MenuItem>
-              <MenuItem eventKey="2">Newest</MenuItem>
-              <MenuItem eventKey="3">Most Rated</MenuItem>
-              <MenuItem eventKey="4">Trending</MenuItem>
-            </DropdownButton>
-          </Col>
-          <Col md={8} >
-            <form >
-              <FormGroup >
-                <FormControl type="text" placeholder="Search"/>
-                <FormControl.Feedback>
-                  <Glyphicon glyph="search" />
-                </FormControl.Feedback>
-              </FormGroup>
-            </form>
-          </Col>
-          <Col md={2} >
-            { this.state.showLogin ? <LoginModal active="login">Login / Sigin Up </LoginModal> : null }
-          </Col>
-        </div>
+      <Row className="submenu">
+        <Col md={2}>
+          <DropdownButton title="Filter Posts" id="bg-nested-dropdown">
+            <MenuItem eventKey="1">All</MenuItem>
+            <MenuItem eventKey="2">Newest</MenuItem>
+            <MenuItem eventKey="3">Most Rated</MenuItem>
+            <MenuItem eventKey="4">Trending</MenuItem>
+          </DropdownButton>
+        </Col>
+        <Col md={8}>
+          <form >
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+              <FormControl.Feedback>
+                <Glyphicon glyph="search" />
+              </FormControl.Feedback>
+            </FormGroup>
+          </form>
+        </Col>
+        <Col md={2}>
+          { this.state.showLogin ? <LoginModal active="login">Login / Sigin Up </LoginModal> : null }
+        </Col>
+      </Row>
     )
   }
 }
