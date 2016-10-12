b570477475672b8e0a4db5855ddd36ac4bc833d9
andela-lolo <loice.andia@andela.com>
2016-10-12 12:20:31 +0300

    [fix #131069477] Resolve merge conflict on community/urls.py

--- codango/community/urls.py
+++ codango/community/urls.py
@@ -1,17 +1,19 @@
 from community import views
 from django.conf.urls import url
 
+
 urlpatterns = [
     url(r'^$', views.CommunityListView.as_view(), name='community_list'),
     url(r'^create$',
         views.CommunityCreateView.as_view(), name='community_create'),
     url(r'^(?P<community_id>[0-9]+)$',
         views.CommunityDetailView.as_view(), name='community_detail'),
+    url(r'^(?P<community_id>[0-9]+)/members$',
+        views.CommunityMemberListView.as_view(), name='community_member'),
     url(r'^create_addon$',
         views.AddOnCreateView.as_view(), name='create_addon'),
     url(r'^list_addons$',
         views.AddOnListView.as_view(), name='addon_list'),
     url(r'^list_addons/(?P<addon_id>[0-9]+)$',
         views.AddOnDetailView.as_view(), name='addon_detail'),
-
 ]
