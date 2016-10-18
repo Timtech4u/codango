2df44dabd3bfdd16bd1e72296c16e7af6a74cb07
andela-lolo <loice.andia@andela.com>
2016-10-18 16:25:19 +0300

    [Fix 131069857] Fix failing tests

--- codango/community/views.py
+++ codango/community/views.py
@@ -1,14 +1,76 @@
+from django.contrib import messages
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import redirect_to_login
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse_lazy
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.utils.encoding import force_text
 from django.views.generic import TemplateView
-from resources.views import LoginRequiredMixin
+
 from .forms import CommunityForm
 from .models import Community, CommunityMember, AddOn
-from django.views.generic.edit import CreateView
-from django.views.generic.list import ListView
-from django.contrib.auth.models import User
-from django.contrib import messages
-from django.http import HttpResponse, HttpResponseNotFound
+from resources.views import LoginRequiredMixin
+
+
+class UserPassesTestMixin(object):
+    """
+    CBV Mixin that allows you to define a test function which must return True
+    if the current user can access the view.
+    """
+
+    login_url = reverse_lazy('login')
+    permission_denied_message = 'You do not have permission to this resource'
+    raise_exception = False
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    def get_login_url(self):
+        """
+        Override this method to override the login_url attribute.
+        """
+        login_url = self.login_url or settings.LOGIN_URL
+        if not login_url:
+            raise ImproperlyConfigured(
+                '{0} is missing the login_url attribute. Define {0}.login_url, settings.LOGIN_URL, or override '
+                '{0}.get_login_url().'.format(self.__class__.__name__)
+            )
+        return force_text(login_url)
+
+    def get_permission_denied_message(self):
+        """
+        Override this method to override the permission_denied_message attribute.
+        """
+        return self.permission_denied_message
+
+    def get_redirect_field_name(self):
+        """
+        Override this method to override the redirect_field_name attribute.
+        """
+        return self.redirect_field_name
+
+    def handle_no_permission(self):
+        if self.raise_exception:
+            raise PermissionDenied(self.get_permission_denied_message())
+        raise PermissionDenied(self.get_permission_denied_message())
+
+    def test_func(self):
+        community = Community.objects.get(pk=self.kwargs.get('community_id'))
+        return community.creator.id == self.request.user.id
+
+    def get_test_func(self):
+        """
+        Override this method to use a different test_func method.
+        """
+        return self.test_func
+
+    def dispatch(self, request, *args, **kwargs):
+        import ipdb
+        ipdb.set_trace()
+        user_test_result = self.get_test_func()()
+        if not user_test_result:
+            return self.handle_no_permission()
+        return super(UserPassesTestMixin, self).dispatch(request, *args, **kwargs)
 
 
 class CommunityCreateView(LoginRequiredMixin, TemplateView):
@@ -126,7 +188,7 @@ class CommunityMemberListView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class AddOnListView(LoginRequiredMixin, TemplateView):
+class AddOnListView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'community/addon_list.html'
 
     def get_context_data(self, **kwargs):
