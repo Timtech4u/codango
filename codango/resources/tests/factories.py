88b56c0da96bf0db82c73dc79d49b0ee6de0d13a
andela-lolo <loice.andia@andela.com>
2016-11-02 10:33:17 +0300

    [Feature #132569383] create factories for the resources model

--- codango/resources/tests/factories.py
+++ codango/resources/tests/factories.py
@@ -1,5 +1,6 @@
+import datetime
 import factory
-
+from community.tests.factories import CommunityFactory, UserFactory
 from resources import models
 
 
@@ -7,7 +8,20 @@ class ResourceFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Resource
 
+    author = factory.RelatedFactory(UserFactory)
+    community = factory.RelatedFactory(CommunityFactory)
+    text = 'This is a sample resource text'
+    language_tags = 'PYTHON'
+    date_added = factory.LazyAttribute(
+        lambda o: o.datetime.now() - datetime.timedelta(hours=1))
+    date_modified = datetime.now()
+
 
 class NotificationQueueFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.NotificationQueue
+
+    user = factory.RelatedFactory(UserFactory)
+    notification_type = "Notification Type"
+    first_interaction = "first interaction"
+    count = 1
